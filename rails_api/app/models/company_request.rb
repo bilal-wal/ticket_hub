c5cfccd2164dff0db3f66531c3729e7b5cd7bd01
bilalbash <bilal.basharat@gmail.com>
2016-07-12 15:42:54 +0500

    added validations to company requests

--- rails_api/app/models/company_request.rb
+++ rails_api/app/models/company_request.rb
@@ -1,2 +1,6 @@
 class CompanyRequest < ApplicationRecord
+  # TODO: custom validate the uniqueness of the company name and user email
+  validates :name, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true, email: true
+  validates :admin_name, presence: true
 end
